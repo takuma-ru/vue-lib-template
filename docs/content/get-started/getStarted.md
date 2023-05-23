a931bce9d007f248f28ccfbacec9b7fbda127752
takuma-ru <kanekotakuma@team-lab.com>
2023-05-23 18:49:24 +0900

    ✨ docs project

---
+++ docs/content/get-started/getStarted.md
@@ -0,0 +1,7 @@
+::page
+---
+title: 'Get Started with Nuxt3'
+descriptions: 'with NuxtJs'
+---
+aaa
+::
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: "@takuma-ru/vue-library-docs",
+  vueLibraryDocs: {
+    isUsePlugin: false,
+  },
+});
